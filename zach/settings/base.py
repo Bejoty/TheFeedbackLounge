91cae31c26c80c7d4576178fa572355d2e78fc26
Austin Hedeen <austin.hedeen@gmail.com>
2015-04-07 13:09:30 -0700

    Install & config Zinnia (blog app)

--- zach/settings/base.py
+++ zach/settings/base.py
@@ -26,11 +26,16 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'zach',
     'base',
     'stream',
     'twitchauth',
     'twitch_profiles',
+    'django_comments',
+    'mptt',
+    'tagging',
+    'zinnia',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -42,6 +47,20 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+    'zinnia.context_processors.version',
+)
+
+SITE_ID = 1
 
 ROOT_URLCONF = 'zach.urls'
 
