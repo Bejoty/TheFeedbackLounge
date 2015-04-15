b7e19def19f827b4e33804a85f7f7197758c0b23
Austin Hedeen <austin.hedeen@gmail.com>
2015-04-15 14:35:31 -0700

    Change logo storage to URL string

--- twitch_profiles/models.py
+++ twitch_profiles/models.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User)
     display_name = models.CharField(max_length=25)
-    logo = models.ImageField(upload_to='profile')
+    logo_url = models.URLField()
     bio = models.CharField(max_length=300)
     access_token = models.CharField(max_length=50)
 
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+      return self.display_name
