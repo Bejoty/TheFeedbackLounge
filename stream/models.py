2a6dcabf9dfb6c1d4abd67cd679441d093413ce3
Austin Hedeen <austin.hedeen@gmail.com>
2015-04-15 14:36:07 -0700

    Add Channel model

--- stream/models.py
+++ stream/models.py
@@ -17,3 +17,24 @@ class Stream(models.Model):
   end = models.TimeField(null=True, blank=True)
 
   description = models.TextField(default="", blank=True)
+
+
+class Channel(models.Model):
+  name = models.CharField(max_length=25)
+  display_name = models.CharField(max_length=25)
+  access_token = models.CharField(max_length=50)
+
+  status = models.CharField(max_length=50)
+  game = models.CharField(max_length=50)
+  views = models.IntegerField()
+  followers = models.IntegerField()
+
+  url = models.URLField()
+  logo_url = models.URLField()
+  video_banner_url = models.URLField()
+  profile_baner_url = models.URLField()
+
+  last_updated = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.display_name
