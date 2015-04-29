6646cbfe11cd394692326d5929626f398e72f102
Austin Hedeen <austin.hedeen@gmail.com>
2015-04-28 18:27:12 -0700

    Add naive news archive to stream index

--- stream/views.py
+++ stream/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Channel
+from zinnia.models import Entry
 
 
 def index(request):
   channel = get_object_or_404(Channel, name='mic_feedback')
   if not channel.recently_updated():
     channel.update()
-  return render(request, 'stream/index.html', {'channel': channel})
+  latest_news = Entry.objects.order_by('-creation_date')[:5]
+  return render(request, 'stream/index.html', {'channel': channel, 'latest_news': latest_news})
 
 def new(request):
   return render(request, 'stream/new.html')
