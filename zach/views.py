d0cdc579e6dffbdf63055c933dfe61d489b30f48
Austin Hedeen <austin.hedeen@gmail.com>
2015-07-25 23:02:41 -0700

    Create new landing page devoted to stream

--- zach/views.py
+++ zach/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from stream.models import Channel
+from zinnia.models import Entry
 
 
 def index(request):
-	return render(request, 'zach/index.html')
+  channel = get_object_or_404(Channel, name='mic_feedback')
+  if not channel.recently_updated():
+    channel.update()
+  latest_news = Entry.objects.order_by('-creation_date')[:5]
+  return render(request, 'zach/new.html', {'channel': channel, 'latest_news': latest_news})
+  
