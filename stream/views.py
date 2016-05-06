750ef811a15f569eca8b0431e1ddb4c3f8987132
Austin Hedeen <austin.hedeen@gmail.com>
2016-05-06 16:42:00 -0700

    Remove view instances of Zinnia in prep for new blog app

--- stream/views.py
+++ stream/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Channel
-from zinnia.models import Entry
 
 
 def index(request):
     channel = get_object_or_404(Channel, name='mic_feedback')
     if not channel.recently_updated():
         channel.update()
-    latest_news = Entry.objects.order_by('-creation_date')[:5]
-    return render(request, 'stream/index.html', {'channel': channel, 'latest_news': latest_news})
+    # latest_news = Entry.objects.order_by('-creation_date')[:5]
+    return render(request, 'stream/index.html', {'channel': channel})
 
 def new(request):
     return render(request, 'stream/new.html')
--- zach/views.py
+++ zach/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import get_object_or_404, render
 
 from stream.models import Channel
-from zinnia.models import Entry
 
 
 def index(request):
     channel = get_object_or_404(Channel, name='mic_feedback')
     if not channel.recently_updated():
         channel.update()
-    latest_news = Entry.objects.order_by('-creation_date')[:5]
-    return render(request, 'zach/new.html', {'channel': channel, 'latest_news': latest_news})
+    # latest_news = Entry.objects.order_by('-creation_date')[:5]
+    return render(request, 'zach/new.html', {'channel': channel})
